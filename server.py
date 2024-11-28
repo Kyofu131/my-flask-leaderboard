bf65e778ff065241dcbc681d7ad31f8484bd730d
Kyofu131 <jhorenbernardo321@gmail.com>
2024-11-28 20:36:38 +0800

    Updated requirements and server configuration for Render deployment

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory
 from flask_cors import CORS
-from waitress import serve
 import os
 
 app = Flask(__name__)
@@ -10,10 +9,12 @@ leaderboard = []
 
 @app.route('/')
 def home():
+    app.logger.debug('Home route called')
     return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
+    app.logger.debug('Favicon route called')
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 @app.route('/submit_score', methods=['POST'])
@@ -21,6 +22,7 @@ def submit_score():
     data = request.get_json()
     name = data.get('name')
     score = data.get('score')
+    app.logger.debug(f'Submit score called with name={name} and score={score}')
     if name and score is not None:
         leaderboard.append({'name': name, 'score': score})
         return jsonify({'message': 'Score submitted successfully!'}), 200
@@ -28,8 +30,9 @@ def submit_score():
 
 @app.route('/leaderboard', methods=['GET'])
 def get_leaderboard():
+    app.logger.debug('Leaderboard route called')
     sorted_leaderboard = sorted(leaderboard, key=lambda x: x['score'], reverse=True)
     return jsonify(sorted_leaderboard)
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
