9a5fcbdec3b1aac7e4e965a6688bd1665d2e5109
Kyofu131 <jhorenbernardo321@gmail.com>
2024-11-28 18:49:55 +0800

    Fix home route

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, jsonify
 from flask_cors import CORS
 from waitress import serve
-import os
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 CORS(app)
 
 leaderboard = []
@@ -11,10 +10,6 @@ leaderboard = []
 @app.route('/')
 def home():
     return "Welcome to the Flask Leaderboard App!"
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 @app.route('/submit_score', methods=['POST'])
 def submit_score():
